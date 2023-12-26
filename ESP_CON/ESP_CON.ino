2ff1c7c4d66a797f068eb9879ba7bb959352adc5
Đặng Đình Khánh <148210279+Khanh-embedded247@users.noreply.github.com>
2023-12-26 23:36:44 +0700

    Update ESP_CON.ino

--- ESP_CON/ESP_CON.ino
+++ ESP_CON/ESP_CON.ino
@@ -5,84 +5,156 @@
 #include <LoRa.h>
 #include <ArduinoJson.h>
 
-// Pin configuration
-#define DHT_PIN 25      // DHT11 data pin
-#define MQ135_PIN 34    // MQ135 analog pin
-#define FAN_PIN 14      // Pin controlling the fan
-#define LIGHT_PIN 12    // Pin controlling the light
+#define DHT_PIN 15
+#define MQ135_PIN 17
+#define FAN_PIN 27
+#define LIGHT_PIN 14
 
-// LoRa configuration
-#define LORA_SS 5       // LoRa chip select
-#define LORA_RST 14     // LoRa reset
-#define LORA_DIO0 2      // LoRa DIO0
-#define LORA_FREQUENCY 433E6  // LoRa frequency
+#define LORA_SS 5
+#define LORA_RST 4
+#define LORA_DIO0 21
+#define LORA_FREQUENCY 433E6
 
-// LoRa packet structure
 struct SensorData {
-  String id;
-  float temp;
-  float hum;
-  int mq;
-  bool fan;
-  bool light;
+    String id;
+    String master;
+    float temp;
+    float hum;
+    int mq;
+    bool fan;
+    bool light;
 };
 
-// Create instances of the sensors
 DHT dht(DHT_PIN, DHT11);
 MQ135 mq135(MQ135_PIN);
 
+SensorData sensorData;  // Giữ cấu trúc dữ liệu để tránh tạo lại
+unsigned long previousMillis = 0;
+const unsigned long interval = 3000;  // Thời gian giữa các lần đọc và gửi dữ liệu
+
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // Initialize LoRa
-  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
-  if (!LoRa.begin(LORA_FREQUENCY)) {
-    Serial.println("LoRa initialization failed. Check your connections.");
-    while (1);
-  }
+    LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+    if (!LoRa.begin(LORA_FREQUENCY)) {
+        Serial.println("LoRa initialization failed. Check your connections.");
+        while (1)
+            ;
+    }
 
-  // Initialize sensors
-  dht.begin();
-
-  // Set pin modes for controlling fan and light
-  pinMode(FAN_PIN, OUTPUT);
-  pinMode(LIGHT_PIN, OUTPUT);
+    dht.begin();
+    LoRa.setSignalBandwidth(125E3);
+    LoRa.setSpreadingFactor(12);
+    LoRa.setCodingRate4(5);
+    pinMode(FAN_PIN, OUTPUT);
+    digitalWrite(FAN_PIN, HIGH);
+    pinMode(LIGHT_PIN, OUTPUT);
+    digitalWrite(LIGHT_PIN, HIGH);
 }
 
 void loop() {
-  // Read sensor data
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  int airQuality = mq135.getPPM(); // Use getPPM to get CO2 concentration
+    unsigned long currentMillis = millis();
 
-  // Read fan and light status
-  bool fanStatus = digitalRead(FAN_PIN) == HIGH;
-  bool lightStatus = digitalRead(LIGHT_PIN) == HIGH;
+    if (currentMillis - previousMillis >= interval) {
+        readAndSendData();
+        previousMillis = currentMillis;
+    }
 
-  // Prepare LoRa packet
-  SensorData sensorData;
-  sensorData.id = "ESP_SLAVE_1";  // Set the device ID
-  sensorData.temp = temperature;
-  sensorData.hum = humidity;
-  sensorData.mq = airQuality;
-  sensorData.fan = fanStatus;
-  sensorData.light = lightStatus;
+    handleControlSignal();
+}
 
-  // Send data via LoRa
-  StaticJsonDocument<200> jsonDoc;
-  jsonDoc["id"] = sensorData.id;
-  jsonDoc["temp"] = sensorData.temp;
-  jsonDoc["hum"] = sensorData.hum;
-  jsonDoc["mq"] = sensorData.mq;
-  jsonDoc["fan"] = sensorData.fan;
-  jsonDoc["light"] = sensorData.light;
+void readAndSendData() {
+    readSensorData();
 
-  char buffer[256];
-  size_t n = serializeJson(jsonDoc, buffer);
-  Serial.println(buffer);
-  LoRa.beginPacket();
-  LoRa.write(reinterpret_cast<const uint8_t*>(buffer), n);
-  LoRa.endPacket();
+    // Kiểm tra xem có yêu cầu bật quạt và đèn không
+    digitalWrite(FAN_PIN, sensorData.fan ? LOW : HIGH);
+    digitalWrite(LIGHT_PIN, sensorData.light ? LOW : HIGH);
 
-  delay(5000); // Delay for 5 seconds before sending the next packet
+    // Tạo JSON để gửi lên bộ chủ
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["master"] = sensorData.master;
+    jsonDoc["id"] = sensorData.id;
+    jsonDoc["temp"] = sensorData.temp;
+    jsonDoc["hum"] = sensorData.hum;
+    jsonDoc["mq"] = sensorData.mq;
+    jsonDoc["fan"] = sensorData.fan;
+    jsonDoc["light"] = sensorData.light;
+
+    char buffer[256];
+    size_t n = serializeJson(jsonDoc, buffer);
+    Serial.println(buffer);
+
+    // Gửi dữ liệu lên bộ chủ qua LoRa
+    sendToMaster(buffer, n);
+}
+
+void readSensorData() {
+    sensorData.id = "ESP_SLAVE_2";
+    sensorData.master="MASTER_1";
+    sensorData.temp = dht.readTemperature();
+    sensorData.hum = dht.readHumidity();
+    sensorData.mq = mq135.getPPM();
+}
+
+void handleControlSignal() {
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        char buffer[1024];
+        int i = 0;
+
+        while (LoRa.available()) {
+            char c = LoRa.read();
+            if (c != '\r' && c != '\n') {
+                buffer[i++] = c;
+            }
+        }
+
+        buffer[i] = '\0';
+
+        // Kiểm tra tín hiệu JSON từ bộ chủ
+        DynamicJsonDocument jsonDoc(200);
+        DeserializationError error = deserializeJson(jsonDoc, buffer);
+        if (error) {
+            Serial.println("Failed to parse LoRa control signal");
+            return;
+        }
+
+        // Lấy thông tin từ tín hiệu JSON
+        const char* slaveId = jsonDoc["id"];
+        if (strcmp(slaveId, "ESP_SLAVE_2") == 0) {
+            // Đúng bộ con nhận tín hiệu
+            sensorData.fan = jsonDoc["fan"];
+            sensorData.light = jsonDoc["light"];
+
+            // Gửi trạng thái của thiết bị lên lại bộ chủ
+            sendStatusToMaster();
+        }
+    }
+}
+
+void sendToMaster(const char* buffer, size_t length) {
+    LoRa.beginPacket();
+    LoRa.write(reinterpret_cast<const uint8_t*>(buffer), length);
+    LoRa.endPacket();
+}
+
+void sendStatusToMaster() {
+    readSensorData();  // Cập nhật trạng thái của thiết bị vào cấu trúc dữ liệu
+
+    // Tạo JSON để gửi lên bộ chủ
+    StaticJsonDocument<200> jsonDoc;
+    jsonDoc["id"] = sensorData.id;
+    jsonDoc["temp"] = sensorData.temp;
+    jsonDoc["hum"] = sensorData.hum;
+    jsonDoc["mq"] = sensorData.mq;
+    jsonDoc["fan"] = sensorData.fan;
+    jsonDoc["light"] = sensorData.light;
+
+    char buffer[256];
+    size_t n = serializeJson(jsonDoc, buffer);
+
+    // Gửi dữ liệu lên bộ chủ qua LoRa
+    sendToMaster(buffer, n);
+
+    Serial.println("Sent status to master");
 }
